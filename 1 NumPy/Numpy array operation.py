73cbd4cdefc88e1adacf32f1d6744f3d4d2a0f85
Rishav <rishav.jnit@gmail.com>
2018-05-21 22:38:47 +0530

    Array operation

---
+++ 1 NumPy/Numpy array operation.py
@@ -0,0 +1,110 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+
+
+# In[2]:
+
+
+arr = np.arange(0,11)
+
+
+# In[3]:
+
+
+arr
+
+
+# In[4]:
+
+
+arr + arr 
+
+
+# In[5]:
+
+
+arr - arr
+
+
+# In[7]:
+
+
+arr*arr
+
+
+# In[8]:
+
+
+arr+100
+
+
+# In[9]:
+
+
+arr*100
+
+
+# In[10]:
+
+
+arr - 100
+
+
+# In[12]:
+
+
+arr / arr 
+
+
+# In[13]:
+
+
+1 / arr
+
+
+# In[15]:
+
+
+arr **2
+
+
+# In[16]:
+
+
+np.sqrt(arr)
+
+
+# In[17]:
+
+
+np.exp(arr)
+
+
+# In[18]:
+
+
+np.max(arr)
+
+
+# In[19]:
+
+
+arr.max()
+
+
+# In[20]:
+
+
+np.sin(arr)
+
+
+# In[21]:
+
+
+np.log(arr)
+
