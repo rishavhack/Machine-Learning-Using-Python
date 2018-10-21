fc79e97bb06b65ed178b053f0da4b59c7a325657
Rishav <rishav.jnit@gmail.com>
2018-10-21 20:17:17 +0530

    DataSet Ready

--- Part 1  Data Preprocessing/dataPreprocessingpy.py
+++ Part 1  Data Preprocessing/dataPreprocessingpy.py
@@ -36,3 +36,25 @@ y = labelencoder_X.fit_transform(y)
 #Splitting the dataset into training sete and Test set
 from sklearn.cross_validation import train_test_split
 X_train,X_test,y_train,y_test = train_test_split(X,y,test_size = 0.2,random_state = 0)
+
+#Feature Sacling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
